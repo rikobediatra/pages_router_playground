420fef0f5a1e36563483dc408131cbf305726d8a
rikobediatra <rikobediatra@gmail.com>
2024-05-18 22:09:08 +0700

    fixing undefined data

--- src/pages/product/[product].tsx
+++ src/pages/product/[product].tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import { useRouter } from "next/router";
 import useSWR from "swr";
 
@@ -7,7 +8,7 @@ function DetailProduct() {
 
     const { data, error, isLoading } = useSWR(`/api/product/${query.product}`, fetcher);
 
-    const changePriceToIdr = data.data.price.toLocaleString('id-ID', {
+    const changePriceToIdr = data?.data?.price.toLocaleString('id-ID', {
       style: 'currency',
       currency: 'IDR'
     });
@@ -16,14 +17,14 @@ function DetailProduct() {
     <div className="flex gap-8 py-12 ml-32">
         <div className="w-1/5">
                 <div className="mb-4">
-                    <img src={data.data.image} alt={data.data.name} />
+                    <img src={data?.data?.image} alt={data?.data?.name} />
                 </div>
                 <div className="flex flex-col gap-1">
                     <h4 className="font-bold">
-                        {data.data.category}
+                        {data?.data?.category}
                     </h4>
                     <p className="text-gray-500">
-                        {data.data.name}
+                        {data?.data?.name}
                     </p>
                     <p className="font-bold mt-2">
                         {changePriceToIdr}
